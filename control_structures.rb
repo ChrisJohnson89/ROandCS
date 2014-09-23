4af4efd28f7ec121b4fc1afb347967de2c7bf583
chrisjohnson89 <afckazoo@gmail.com>
2014-09-23 14:26:30 -0700

    completed  control structure

--- control_structures.rb
+++ control_structures.rb
@@ -6,3 +6,13 @@ if name == "Chris"
 else
   puts "Hi #{name}!"
 end
+
+print "Would you like to see your name backwords? (yes or no)"
+answer = gets.chomp.downcase
+
+if answer == "yes"
+  puts "This is your name backwards:"
+  puts name.reverse
+else
+ puts "Ok, maybe later."
+end
